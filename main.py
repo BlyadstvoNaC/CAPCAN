03918eb40e9360e4cb1edabd7fdb8812f54ed96c
igr1k <igr1ks@yandex.ru>
2024-05-19 15:54:18 +0300

    че нибудь

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import MINE
 @bot.message_handler(content_types=['text'])
 def start(message):
-    if message == '\start':
-        pass
-    elif message == "\dmin":
-        pass
+    if message == '/start':
+        user_id = message.from_user.id
+        if user_id not in [list]:
+    elif message == '/profile':
 
 @bot.callback_query_handler(func = lambda call: True)
 def func(call):
-    #
-    MINE.func(call)
+    pass
+
+def regist(user_id):
+    pass
+    #сообщения + отдаем список с введенными данными
