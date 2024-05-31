9d3f8aee554b01cba940e16d8e319f084e477da6
igr1k <igr1ks@yandex.ru>
2024-05-31 20:29:09 +0300

    31/05/2024

--- Client_functions.py
+++ Client_functions.py
@@ -1,8 +1,12 @@
 import telebot
 from KeyBoards import profileMP
-from IgorFuntions import is_valid_email
-from Peremen import user_data, token, command_list
 from DBfunctions import db
+from My.BotToken import token
+import re
+
+
+user_data = {}
+command_list = ['/start', '/profile', '/menu', '/basket', '/history', '/my_orders']
 
 bot = telebot.TeleBot(token)
 
@@ -10,11 +14,10 @@ bot = telebot.TeleBot(token)
 def handle_command(message):
     bot.clear_step_handler_by_chat_id(chat_id=message.chat.id)
     if message.text == '/start':
-        #бог знает как пользоваться
-        if db.is_registered(message.chat.id):
-            regist(message.chat.id)
+        if db.is_registered(message.chat.id) == False:
+            regist(message)
         else:
-            pass # вызываем меню
+            print('menu')
     elif message.text == '/profile':
         set_profile(message)
 
@@ -25,7 +28,6 @@ def regist(message):
                                       "Пожалуйста предоставьте данные для регистрации")
     msg = bot.send_message(message.chat.id, "Как вас зовут:")
     bot.register_next_step_handler(msg, reg_name)
-
 
 def reg_name(message):
     if message.text in command_list:
@@ -46,13 +48,13 @@ def reg_tp_number(message):
     else:
         if len(message.text) == 13 and message.text[:4] == '+375' and message.text[4:6] in ['29', '33', '44']:
             user_data[message.from_user.id].append(message.text)
-            msg = bot.send_message(message.chat.id, 'Какой у вас адрес?')
-            bot.register_next_step_handler(msg, reg_address)
+            msg = bot.send_message(message.chat.id, 'Какой у вас email?')
+            bot.register_next_step_handler(msg, reg_email)
         else:
             msg = bot.send_message(message.chat.id, 'Неверный номер, попробуйте еще раз')
             bot.register_next_step_handler(msg, reg_tp_number)
 
-#поменять местами
+
 def reg_address(message):
     if message.text in command_list:
         bot.send_message(message.chat.id, "Сначала пройдите регистрацию:")
@@ -60,8 +62,16 @@ def reg_address(message):
         bot.register_next_step_handler(msg, reg_address)
     else:
         user_data[message.from_user.id].append(message.text)
-        msg = bot.send_message(message.chat.id, 'Укажите ваш email?')
-        bot.register_next_step_handler(msg, reg_email)
+        bot.send_message(message.chat.id, 'Спасибо за регистрацию!')
+        tmp_list = []
+        tmp_list.append(message.from_user.id)
+        for i in user_data[message.from_user.id]:
+            tmp_list.append(i)
+        print(tmp_list)
+        tmp_list.clear()
+        db.new_client(tmp_list)
+
+        set_profile(message)
 
 def reg_email(message):
     if message.text in command_list:
@@ -71,17 +81,13 @@ def reg_email(message):
     else:
         if is_valid_email(message.text):
             user_data[message.from_user.id].append(message.text)
-            bot.send_message(message.chat.id, 'Спасибо за регистрацию!')
-            print(user_data)
-            set_profile(message)
-
-            #функция добавления в БД
+            msg = bot.send_message(message.chat.id, 'Укажите ваш адрес.')
+            bot.register_next_step_handler(msg, reg_address)
         else:
             msg = bot.send_message(message.chat.id, 'Неверный паттерн почты, попробуйте еще раз')
             bot.register_next_step_handler(msg, reg_email)
 
 #########################################################################################
-
 
 #                                   БЛОК ИЗМЕНЕНИЯ ПРОФИЛЯ                              #
 #########################################################################################
@@ -89,7 +95,8 @@ def reg_email(message):
 def change_profile(callback):
     data = callback.data[3:]
     if data == 'name':
-        print("Изменить время")
+        msg = bot.send_message(callback.message.chat.id,"Ввдеите имя:")
+        bot.register_next_step_handler(msg, change_name)
     elif data == 'tp':
         pass
     elif data == 'address':
@@ -100,14 +107,21 @@ def change_profile(callback):
         pass
 
 def set_profile(message):
-
+    tmp_user_list = db.get_client_data(message.from_user.id)
     bot.send_message(message.chat.id, f"Хотите изменить профиль? \n"
-                                       f"Имя: \n"
-                                      f"Телефон: \n"
-                                      f"Адрес: \n"
-                                      f"Email: \n", reply_markup=profileMP)
+                                       f"Имя: {tmp_user_list[2]}\n"
+                                      f"Телефон: {tmp_user_list[3]}\n"
+                                      f"Email: {tmp_user_list[4]}\n"
+                                      f"Адрес: {tmp_user_list[5]}\n", reply_markup=profileMP)
+
+def change_name(message):
+    pass
 
 
 #########################################################################################
+
+def is_valid_email(email):
+    pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    return re.match(pattern, email) is not None
 
 bot.infinity_polling()
