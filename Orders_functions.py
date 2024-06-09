9d56567ed6283ffafd518e1e66c0aca4210f7691
Fedia <fedia-zubikof@yandex.by>
2024-06-09 11:51:37 +0300

    Обновлен Orders_functions.py

--- Orders_functions.py
+++ Orders_functions.py
@@ -2,7 +2,6 @@ import telebot
 from telebot import types
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-# import Client_functions
 from My import BotToken
 from DBfunctions import db
 from Sheduler import schedule_message, start_scheduler_thread
@@ -36,12 +35,15 @@ list_order_for_bd = []
 На вход принимается история заказов из БД по Ольгиной функции в виде списка кортежей. Что ж за жизнь, то такая?"""
 
 
+# data_history = db.orders_history(message.chat.id)
+
+
 def history_orders(data):
     list_history_orders = []
 
-    for index, history in enumerate(data_history):
+    for index, history in enumerate(data):
         """взять из Оленой функции набор с заказами и вывести их по одному: заказ номер - дата"""
-        res_hystory = f'{history[0]} - {history[3]}'
+        res_hystory = f'{history[0]} - {history[3]}'  # Форматирование строки с номером заказа и датой
         list_history_orders.append(res_hystory)
     return list_history_orders
 
@@ -69,22 +71,23 @@ def get_max_cooking_time(orders):
 # data_history = db.orders_history(message.chat.id)
 # data_history = db.orders_history('3fdf5g544')  # Тест
 """тест для истории заказов?"""
-data_history = [(1, 1, 1, '20240519 15:40:00', 50, 'г Минск, пр Держинского 154'),
-                (2, 1, 1, '20240519 16:40:00', 50, 'г Минск, пр Держинского 84'),
-                (3, 1, 1, '20240519 17:40:00', 50, 'г Минск, пр Держинского 154'),
-                (4, 1, 1, '20240519 18:40:00', 50, 'г Минск, пр Держинского 15'),
-                (5, 1, 1, '20240519 19:40:00', 50, 'г Минск, пр Держинского 54'),
-                (6, 1, 1, '20240519 20:40:00', 50, 'г Минск, пр Держинского 14')]  # Тест
-
-print(history_orders(data_history))
+# data_history = [(1, 1, 1, '20240519 15:40:00', 50, 'г Минск, пр Держинского 154'),
+#                 (2, 1, 1, '20240519 16:40:00', 50, 'г Минск, пр Держинского 84'),
+#                 (3, 1, 1, '20240519 17:40:00', 50, 'г Минск, пр Держинского 154'),
+#                 (4, 1, 1, '20240519 18:40:00', 50, 'г Минск, пр Держинского 15'),
+#                 (5, 1, 1, '20240519 19:40:00', 50, 'г Минск, пр Держинского 54'),
+#                 (6, 1, 1, '20240519 20:40:00', 50, 'г Минск, пр Держинского 14')]  # Тест
+#
+# print(history_orders(data_history))
 
 """Функция `generate_markup(page)` создает клавиатуру с кнопками для навигации по страницам"""
 
 
-def generate_markup(page):
+# def generate_markup(page):
+def generate_markup(page, data_history):
     markup = InlineKeyboardMarkup(row_width=1)
-    start_index = page * 2  # изменение цифры выводимых заказов
-    end_index = start_index + 2  # изменение цифры выводимых заказов
+    start_index = page * 2  # Начальный индекс для отображаемых заказов
+    end_index = start_index + 2  # Конечный индекс для отображаемых заказов
     for item in history_orders(data_history)[start_index:end_index]:
         markup.add(InlineKeyboardButton(item, callback_data="m" + f'{item}'))
     nav_buttons = []
@@ -100,64 +103,72 @@ def generate_markup(page):
 
 
 def check_history(message):
-    if message.text == '/history':
-        bot.send_message(
-            message.chat.id, "Список завершенных заказов.",
-        )
-        bot.send_message(
-            message.chat.id, "Выберите один из заказов ⬇️:",
-            reply_markup=generate_markup(0)
-        )
+    data_history = db.orders_history(message.chat.id)  # Получение истории заказов из БД
+    if not data_history:
+        bot.send_message(message.chat.id, "Истории заказов нет.")
+        return
+    bot.send_message(
+        message.chat.id, "Список завершенных заказов.",
+    )
+    bot.send_message(
+        message.chat.id, "Выберите один из заказов ⬇️:",
+        reply_markup=generate_markup(0, data_history)
+    )
+
+
+"""Функция обработки my_orders"""
+
+
+def check_my_orders(message):
+    data_my_order = db.my_orders(message.chat.id)
+    data_my_client = db.get_client_data(message.chat.id)
+    # data_dishes_my_order = db.dishes_data(history[0])
+    # data_my_order = db.my_orders('nfj4j3nj4')  # Тест
+    # data_my_client = db.get_client_data('nfj4j3nj4')  # Тест
+    # data_dishes_my_order = db.dishes_data(1)  # Тест
+    id_my_order = data_my_order[0][0]  # Да ладно, охренеть
+    print(id_my_order)
+    data_dishes_my_order = db.dishes_data(id_my_order)  # Тест
+    print(data_my_order)
+    print(data_my_client)
+
+    """проверка на наличие текущего заказа в БГ"""
+    # if my_dict_orders["user_tg_chat_id"]:
+    # if user_order_dict[message.chat.id]:
+    if data_my_order[0][2] == 1:
+        bot.send_message(message.chat.id, "Ваш заказ вроде доставлен. Хз")
+        return
+
+    bot.send_message(message.chat.id, "Текущий заказ ⬇️")
+    """выводим данные о клиенте?"""
+    """взять их из БД? Вывели"""
+    """данные клиента по заказу: имя, телефон, почта, адрес"""
+    # for user in data_my_client:
+    data_user = f'{data_my_client[2]}\n{data_my_client[3]}\n{data_my_client[4]}\n{data_my_client[5]}'
+    bot.send_message(message.chat.id, "Данные о клиенте")
+    bot.send_message(message.chat.id, data_user)
+
+    print(data_dishes_my_order)
+    """выводим список блюд (тоже из бд c 0 в is_delivered по id клаента)?"""
+    """взять их из откуда(из БД по def dishes_data(order_id): наверное. Чистое ХУдожество)?"""
+    bot.send_message(message.chat.id, "Список блюд и общая сумма заказа:")
+    res_sum = 0
+    res_dish_hist = ''
+    for dish in data_dishes_my_order:
+        print(dish)
+        # for dish in data_dishes_my_order:
+        res_dish_hist += f'{dish[1]}\n'
+        res_sum += dish[2] * dish[3]
+    bot.send_message(message.chat.id, res_dish_hist)
+    bot.send_message(message.chat.id, (str(res_sum) + " руб."), reply_markup=markupH)
 
 
 def start(message):
     if message.text == '/my_orders':
-        # """проверка на наличие текущего заказа"""
-        # if my_dict_orders["user_tg_chat_id"]:
-        # if user_order_dict[message.chat.id]:
-        bot.send_message(message.chat.id, "Текущий заказ ⬇️")
-        """выводим данные о клиенте?"""
-        """взять их из БД? Вывели"""
-        # data_my_order = db.my_orders(message.chat.id)
-        # data_my_client = db.get_client_data(message.chat.id)
-        # data_dishes_my_order = db.dishes_data(history[0])
-        data_my_order = db.my_orders('nfj4j3nj4')  # Тест
-        data_my_client = db.get_client_data('nfj4j3nj4')  # Тест
-        data_dishes_my_order = db.dishes_data(1)  # Тест
-
-        """данные клиента по заказу: имя, телефон, почта, адрес"""
-        """Выводим кнопки"""
-        # for user in data_my_client:
-        data_user = f'{data_my_client[2]}\n{data_my_client[3]}\n{data_my_client[4]}\n{data_my_client[5]}'
-        bot.send_message(message.chat.id, "Данные о клиенте")
-        bot.send_message(message.chat.id, data_user)
-
-        print(data_my_order)
-        print(data_my_client)
-        print(data_dishes_my_order)
-        """выводим список блюд (тоже из бд или из корзины)?"""
-        """взять их из откуда(БД)?"""
-        bot.send_message(message.chat.id, "Список блюд и общая сумма заказа:")
-        res_sum = 0
-        res_dish_hist = ''
-        for dish in data_dishes_my_order:
-            res_dish_hist += f'{dish[1]}\n'
-            res_sum += dish[2] * dish[3]
-        bot.send_message(message.chat.id, res_dish_hist)
-        bot.send_message(message.chat.id, (str(res_sum) + " руб."), reply_markup=markupH)
+        check_my_orders(message)
 
     if message.text == '/history':
         check_history(message)
-
-
-    # if message.text == '/history':
-    #     bot.send_message(
-    #         message.chat.id, "Список завершенных заказов.",
-    #     )
-    #     bot.send_message(
-    #         message.chat.id, "Выберите один из заказов ⬇️:",
-    #         reply_markup=generate_markup(0)
-    #     )
 
     if message.text == '/basket':
         send_basket(message.chat.id)
@@ -169,8 +180,13 @@ def start(message):
 
 
 def send_basket(chat_id):
+    # user_order_dict={6142885563:[(2, 'Омлет', 2, 17.0)]}
+    # print(user_order_dict)
+    if not user_order_dict:
+        bot.send_message(chat_id, "Корзина пуста.")
+        return
     if user_order_dict[chat_id]:
-    # if my_dict_orders["user_tg_chat_id"]:
+        # if my_dict_orders["user_tg_chat_id"]:
         markupB = InlineKeyboardMarkup(row_width=2)
         button_1 = InlineKeyboardButton('Изменить', callback_data='change')
         button_2 = InlineKeyboardButton('Продолжить', callback_data='continue')
@@ -179,7 +195,7 @@ def send_basket(chat_id):
         res_sum_bask = 0
         res_dish_bask = ''
         for dish_bask in user_order_dict[chat_id]:
-        # for dish_bask in my_dict_orders["user_tg_chat_id"]:
+            # for dish_bask in my_dict_orders["user_tg_chat_id"]:
             res_dish_bask += f'Блюдо {dish_bask[1]} {dish_bask[3]} - {dish_bask[2]} - {dish_bask[2] * dish_bask[3]}\n'
             res_sum_bask += dish_bask[2] * dish_bask[3]
 
@@ -229,7 +245,7 @@ def confirm_order(message):
 
     # Парафин для БД
     for dish_bask in user_order_dict[message.chat.id]:
-    # for dish_bask in my_dict_orders["user_tg_chat_id"]:
+        # for dish_bask in my_dict_orders["user_tg_chat_id"]:
         res_order_for_bd = [dish_bask[0], dish_bask[2]]
         list_order_for_bd.append(res_order_for_bd)
     print(list_order_for_bd)
@@ -239,6 +255,7 @@ def confirm_order(message):
     time_for_bd = (time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(schedule_time)))
     print(message.chat.id, time_for_bd)
     # db.new_order('nfj4j3nj4', str(time_for_bd), list_order_for_bd)
+    # запись в БД под видом нового заказа. Вроде протестено, да здесь вообще все протестено:
     db.new_order(message.chat.id, time_for_bd, list_order_for_bd)
 
 
@@ -260,6 +277,7 @@ def query_handler(call):
     flag = call.data[0]
     data = call.data[1:]
     if flag == "m":
+
         bot.send_message(call.message.chat.id, f"Выбранный из истории заказ ⬇️:  {data}")
         """выводим данные о клиенте?"""
         """взять их из БД? Вывели"""
@@ -289,11 +307,16 @@ def query_handler(call):
         bot.send_message(call.message.chat.id, res_dish_hist)
         bot.send_message(call.message.chat.id, (str(res_sum) + " руб."))
 
+
     elif flag == "<" or flag == ">":
         page = int(data)
-
+        data_history = db.orders_history(call.message.chat.id)  # Получение истории заказов из БД
+        if not data_history:
+            bot.edit_message_text("Истории заказов нет.", call.message.chat.id, call.message.message_id)
+            return
         bot.edit_message_text("Выберите один из заказов ⬇️", call.message.chat.id, call.message.message_id,
-                              reply_markup=generate_markup(page))
+                              reply_markup=generate_markup(page, data_history))
+
 
     elif flag == "h":
         # Обработка команды Help
@@ -304,13 +327,13 @@ def query_handler(call):
     elif flag == "o":
         # Обработка команды Заказ подтвержден
         data_my_order = db.my_orders(call.message.chat.id)
-        data_my_client = db.get_client_data(call.message.from_user.id)
+        print(data_my_order)
+        data_my_client = db.get_client_data(call.message.chat.id)
         # data_my_order = db.my_orders('nfj4j3nj4')  # Тест
 
         """проверка на из-деливерид?"""
         if data_my_order[0][2] == 0:
-            bot.send_message(call.message.chat.id, "Доставлен ли заказ? Оставьте, пожалуйста, комментарий: "
-                                                   "https://www.instagram.com")
+            bot.send_message(call.message.chat.id, "Ваш заказ еще не доставлен.")
 
         elif data_my_order[0][2] == 1:
             bot.send_message(call.message.chat.id, "Заказ завершен. Оставьте, пожалуйста, комментарий: "
@@ -328,7 +351,7 @@ def query_handler(call):
     if call.data == 'change':
         markupUB = InlineKeyboardMarkup(row_width=4)
         for ind_but, dish_but in enumerate(user_order_dict[call.message.chat.id]):
-        # for ind_but, dish_but in enumerate(my_dict_orders["user_tg_chat_id"]):
+            # for ind_but, dish_but in enumerate(my_dict_orders["user_tg_chat_id"]):
             res_upd_dish_bask = f'{dish_but[1]} - {dish_but[2]}'
             button_1 = InlineKeyboardButton('🗑', callback_data=f'delete_{dish_but[0]}')
             button_2 = InlineKeyboardButton('-', callback_data=f'decrease_{dish_but[0]}')
@@ -343,12 +366,12 @@ def query_handler(call):
 
     elif call.data == 'continue':
         """Проверка зарегистрирован ли пользователь"""
-        if db.is_registered(call.message.from_user.id):
-        # if db.is_registered('3fdf5g544'):
+        if db.is_registered(call.message.chat.id):
+            # if db.is_registered('3fdf5g544'):
             """выводим данные о клиенте?"""
             """взять их из БД? Вывели"""
 
-            data_my_client = db.get_client_data(call.message.from_user.id)
+            data_my_client = db.get_client_data(call.message.chat.id)
             # data_my_client = db.get_client_data('3fdf5g544')
             # data_my_order = db.my_orders('3fdf5g544')
             data_dishes_my_order = db.dishes_data(1)
@@ -365,7 +388,7 @@ def query_handler(call):
             res_sum_bask = 0
             res_dish_bask = ''
             for dish_bask in user_order_dict[call.message.chat.id]:
-            # for dish_bask in my_dict_orders["user_tg_chat_id"]:
+                # for dish_bask in my_dict_orders["user_tg_chat_id"]:
                 res_dish_bask += f'{dish_bask[1]}\n'
                 res_sum_bask += dish_bask[2] * dish_bask[3]
 
@@ -376,7 +399,8 @@ def query_handler(call):
         """функция удаления блюда в корзине"""
     elif call.data.startswith('delete_'):
         dish_id = int(call.data.split('_')[1])
-        user_order_dict[call.message.chat.id] = [dish for dish in user_order_dict[call.message.chat.id] if dish[0] != dish_id]
+        user_order_dict[call.message.chat.id] = [dish for dish in user_order_dict[call.message.chat.id] if
+                                                 dish[0] != dish_id]
         # my_dict_orders["user_tg_chat_id"] = [dish for dish in my_dict_orders["user_tg_chat_id"] if dish[0] != dish_id]
         bot.send_message(call.message.chat.id, "Блюдо удалено из корзины.")
         # print(my_dict_orders["user_tg_chat_id"])
@@ -386,7 +410,7 @@ def query_handler(call):
     elif call.data.startswith('decrease_'):
         dish_id = int(call.data.split('_')[1])
         for i, dish in enumerate(user_order_dict[call.message.chat.id]):
-        # for i, dish in enumerate(my_dict_orders["user_tg_chat_id"]):
+            # for i, dish in enumerate(my_dict_orders["user_tg_chat_id"]):
             if dish[0] == dish_id:
                 if dish[2] > 1:
                     dish_list = list(dish)
@@ -398,14 +422,13 @@ def query_handler(call):
                     # my_dict_orders["user_tg_chat_id"].pop(i)
                 break
         bot.send_message(call.message.chat.id, "Количество блюда уменьшено.")
-        # print(my_dict_orders["user_tg_chat_id"])
         send_basket(call.message.chat.id)
 
         """функция добавления кол-ва блюд в корзине"""
     elif call.data.startswith('increase_'):
         dish_id = int(call.data.split('_')[1])
         for i, dish in enumerate(user_order_dict[call.message.chat.id]):
-        # for i, dish in enumerate(my_dict_orders["user_tg_chat_id"]):
+            # for i, dish in enumerate(my_dict_orders["user_tg_chat_id"]):
             if dish[0] == dish_id:
                 dish_list = list(dish)
                 dish_list[2] += 1
@@ -435,18 +458,22 @@ if __name__ == "__main__":
 
     @bot.message_handler(content_types=['text'])
     def message_handler(message):
-        try:
-            start(message)
-        except Exception as e:
-            logging.error(f"Error in message_handler: {e}")
+        start(message)
+        # try:
+        #     start(message)
+        # except Exception as e:
+        #     logging.error(f"Error in message_handler: {e}")
 
 
     @bot.callback_query_handler(func=lambda call: True)
     def callback_handler(call):
-        try:
-            query_handler(call)
-        except Exception as e:
-            logging.error(f"Error in callback_handler: {e}")
+
+        query_handler(call)
+
+        # try:
+        #     query_handler(call)
+        # except Exception as e:
+        #     logging.error(f"Error in callback_handler: {e}")
 
 
     # while True:
